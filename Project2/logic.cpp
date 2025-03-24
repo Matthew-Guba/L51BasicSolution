e8e32a5f58ea1efe418a3265568b908f618438a3
Matthew-Guba <chamistrygeorge3@gmail.com>
2025-03-24 18:57:23 +0300

    fool-proofed

--- Project2/logic.cpp
+++ Project2/logic.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int fibonacci(int n) {
+    if (n < 0) return -1;
     if (n <= 1) {
         return n;
     }
--- Project2/main.cpp
+++ Project2/main.cpp
@@ -6,7 +6,10 @@ int main() {
     int number;
     cout << "Enter the Fibonacci sequence position: ";
     cin >> number;
-
+    if (fibonacci(number) == -1) {
+        cout << "It has a mistake";
+        return -1;
+    }
     cout << "The Fibonacci number at position " << number << " is " << fibonacci(number) << endl;
 
     return 0;
